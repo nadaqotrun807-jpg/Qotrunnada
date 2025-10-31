809ce664a3595fa8de7d9d4ca4ddf6e973190fe8
nadaqotrun807-jpg <nadaqotrun807@gmail.com>
2025-10-31 20:59:38 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.set_page_config(page_title="RSA–AES Key Wrap", page_icon="🔐", layout="centered")
+st.title("🔐 Simulasi Enkripsi Kunci AES-128 dengan RSA-OAEP")
+st.caption("Langkah 1: pastikan halaman ini tampil. Kalau ini terlihat, UI Streamlit berfungsi.")
